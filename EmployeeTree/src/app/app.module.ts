8b34e22a3d1694ffd3453760e9a0e84b9c2005f6
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-08-03 09:53:56 +0200

    :sparkles: (UI) : Generate components

--- EmployeeTree/src/app/app.module.ts
+++ EmployeeTree/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EmployeeCardComponent } from './components/employee-card/employee-card.component';
+import { ScheduleCardComponent } from './components/schedule-card/schedule-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    EmployeeCardComponent,
+    ScheduleCardComponent
   ],
   imports: [
     BrowserModule,
